4a279117c4a42f6f0c724f200ee1f834fd4924eb
Vinicius Oliveira <vinioliveiracontato03@gmail.com>
2023-03-16 22:53:32 -0300

    First Commit

---
+++ src/main/java/br/com/checkpoint/CheckPoint1EAD/CheckPoint1EadApplication.java
@@ -0,0 +1,13 @@
+package br.com.checkpoint.CheckPoint1EAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckPoint1EadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckPoint1EadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/checkpoint/CheckPoint1EAD/CheckPoint1EadApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.checkpoint.CheckPoint1EAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckPoint1EadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
